5a70cbd0523a0897fe9b03be229d515a0901d2df
Kim Herala <14854225+kimherala@users.noreply.github.com>
2022-07-19 23:55:42 +0300

    Added basic SDL game loop
    No real logic here yet, just a empty window.

--- src/game.zig
+++ src/game.zig
@@ -1,6 +1,9 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 const World = @import("world.zig").World;
+const sdl = @cImport({
+    @cInclude("SDL.h");
+});
 
 pub const Game = struct {
     const Self = @This();
@@ -14,6 +17,29 @@ pub const Game = struct {
     }
 
     pub fn loop(self: Game) !void {
+        _ = sdl.SDL_Init(sdl.SDL_INIT_VIDEO);
+        defer sdl.SDL_Quit();
+
+        var window = sdl.SDL_CreateWindow("Rogue", sdl.SDL_WINDOWPOS_CENTERED, sdl.SDL_WINDOWPOS_CENTERED, 640, 400, 0);
+        defer sdl.SDL_DestroyWindow(window);
+
+        var renderer = sdl.SDL_CreateRenderer(window, 0, sdl.SDL_RENDERER_PRESENTVSYNC);
+        defer sdl.SDL_DestroyRenderer(renderer);
+
+        gameloop: while (true) {
+            var sdl_event: sdl.SDL_Event = undefined;
+            while (sdl.SDL_PollEvent(&sdl_event) != 0) {
+                switch (sdl_event.type) {
+                    sdl.SDL_QUIT => break :mainloop,
+                    else => {},
+                }   
+            }
+            
+            _ = sdl.SDL_SetRenderDrawColor(renderer, 0xff, 0xff, 0xff, 0xff);
+            _ = sdl.SDL_RenderClear(renderer);
+            sdl.SDL_RenderPresent(renderer);
+        }
+
         self.world.update();
     }
 };
